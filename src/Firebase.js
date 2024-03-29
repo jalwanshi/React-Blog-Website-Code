62250e42549a9b358e0367205766e1893b7fc922
Ankit Nishad <146050586+jalwanshi@users.noreply.github.com>
2024-03-29 14:01:36 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,22 @@
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
-import "firebase/compat/firestore"
-import "firebase/compat/database"
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/database";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC2xeIsUfJyrwCz6rZmsXz_yI5vtm1CfzA",
-  authDomain: "uploadnews-85bb0.firebaseapp.com",
-  databaseURL: "https://uploadnews-85bb0-default-rtdb.firebaseio.com",
-  projectId: "uploadnews-85bb0",
-  storageBucket: "uploadnews-85bb0.appspot.com",
-  messagingSenderId: "648630406044",
-  appId: "1:648630406044:web:1366f33e2ecfe3102ad659"
+  apiKey: "Your-API-Key",
+  authDomain: "Your-Auth-Domain",
+  databaseURL: "Your-Database-URL",
+  projectId: "Your-Project-ID",
+  storageBucket: "Your-Storage-Bucket",
+  messagingSenderId: "Your-Messaging-Sender-ID",
+  appId: "Your-App-ID"
 };
 
 // Initialize Firebase
- const Firebase= firebase.initializeApp(firebaseConfig);
- export default Firebase.database().ref()
+const Firebase = firebase.initializeApp(firebaseConfig);
 
+export const database = Firebase.database().ref();
+
+export default Firebase;
+// Use Your Firebase Setup Here Thank You
